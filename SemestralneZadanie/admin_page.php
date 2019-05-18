5c3e29fa464ef930ca776b613be602123fbd8ce6
Gergely Czerwinski <gergely.czerwinski@zentity.com>
2019-05-18 09:55:27 +0200

    addedStringsAndStudentsPage

--- SemestralneZadanie/admin_page.php
+++ SemestralneZadanie/admin_page.php
@@ -25,6 +25,7 @@ if(isset($_GET['language'])){
     <link rel="icon" href="data:;base64,=">
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
     <script src="./JS/script.js"></script>
     
     
@@ -52,14 +53,14 @@ if(isset($_GET['language'])){
     </article>
 	
 	<form action="addPredmet.php" method="post" enctype="multipart/form-data">
-	<h3>Pridanie predmetu</h3>
-	Skolsky rok:<br>
+	<h3><div id='pridaniePredmetuText'><script>initText(document.getElementById('pridaniePredmetuText'),'".$page_name[0].".pridaniePredmetu', '".$language."')</script></h3><!-- Pridanie Predmetu -->
+	<div id='skolskyRokText'><script>initText(document.getElementById('skolskyRokText'),'".$page_name[0].".skolskyRok', '".$language."')</script><br><!-- Skolsky rok: -->
 	<input type="text" name="skolsky_rok" id="skolsky_rok"><br>
-	Nazov predmetu:<br>
+	<div id='nazovPredmetuText'><script>initText(document.getElementById('nazovPredmetuText'),'".$page_name[0].".nazovPredmetu', '".$language."')</script><br><!-- Nazov predmetu: --><br>
 	<input type="text" name="nazov_predmetu" id="nazov_predmetu"><br>
-	CSV subor:<br>
+	<div id='CSVSuborText'><script>initText(document.getElementById('CSVSuborText'),'".$page_name[0].".CSVSubor', '".$language."')</script><br><!-- CSV subor: --><br><br>
 	<input type="file" name="csv_subor" id="csv_subor"><br>
-	Oddelovac csv suboru:<br>
+	<div id='oddelovacText'><script>initText(document.getElementById('oddelovacText'),'".$page_name[0].".oddelovac', '".$language."')</script><br><!-- Oddelovac csv suboru: --><br><br><br>
 	<input type="text" name="oddelovac_csv" id="oddelovac_csv"><br>
 	
 	<input type="submit" value="Submit" name="submit"><br>
@@ -67,10 +68,14 @@ if(isset($_GET['language'])){
 	</form>
 	
 	<?php
+	echo '<div id="tables">';
 	$query_select = "SELECT * FROM Predmety;";
 	$result = mysqli_query($conn, $query_select);
+	$number_of_table = 0;
 	while (list($pk, $skolsky_rok, $nazov_predmetu, $csv_subor, $oddelovac_csv) = $result->fetch_row()) {
-		echo $skolsky_rok . "<br>" . '<a href="deletePredmet.php?id=' . $pk . '">' . $nazov_predmetu . '</a>' . " <-odkaz na mazanie" . "<br>";
+		echo $skolsky_rok . "<br>" . '<a href="deletePredmet.php?id=' . $pk . '">' . $nazov_predmetu . '</a>' . "<br>";
+		echo '<input type="button" value="Create PDF" 
+            id="btPrint" onclick="createPDF(' . $number_of_table . ')" />';
 		//$csvArray = str_getcsv($csv_subor);
 		$my_file = 'test.csv';
 		$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
@@ -79,7 +84,7 @@ if(isset($_GET['language'])){
 		fclose($handle);
 		$row = 1;
 		if (($handle = fopen("test.csv", "r")) !== FALSE) {
-			echo "<table>";
+			echo '<table id="' . $number_of_table .'">';
 		while (($data = fgetcsv($handle, 1000, $oddelovac_csv)) !== FALSE) {
 			
 			if($row == 1) {
@@ -107,9 +112,53 @@ if(isset($_GET['language'])){
 		}
 		fclose($handle);
 		echo "<br>";
+		$number_of_table++;
 	}
+	echo "<div>";
 	?>
-    
+	
+	<script>
+	function createPDF(id) {
+	var pdf = new jsPDF('p', 'pt', 'letter');
+            // source can be HTML-formatted string, or a reference
+            // to an actual DOM element from which the text will be scraped.
+			
+            source = $('#tables')[id];
+			window.alert(source);
+            // we support special element handlers. Register them with jQuery-style 
+            // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+            // There is no support for any other type of selectors 
+            // (class, of compound) at this time.
+            specialElementHandlers = {
+                // element with id of "bypass" - jQuery style selector
+                '#bypassme': function(element, renderer) {
+                    // true = "handled elsewhere, bypass text extraction"
+                    return true
+                }
+            };
+            margins = {
+                top: 80,
+                bottom: 60,
+                left: 40,
+                width: 522
+            };
+            // all coords and widths are in jsPDF instance's declared units
+            // 'inches' in this case
+            pdf.fromHTML(
+                    source, // HTML string or DOM elem ref.
+                    margins.left, // x coord
+                    margins.top, {// y coord
+                        'width': margins.width, // max width of content on PDF
+                        'elementHandlers': specialElementHandlers
+                    },
+            function(dispose) {
+                // dispose: object with X, Y of the last line add to the PDF 
+                //          this allow the insertion of new lines after html
+                pdf.save('Test.pdf');
+            }
+            , margins);
+        }
+	</script>
 
     <footer>
         <p>&copy; The Crew 2019</p>
